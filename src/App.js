96b1101a9392306a678c236cc8ede60293f8c8ca
DenisButCheR <denis.butcher@gmail.com>
2019-01-29 20:17:41 +0200

    Init

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Helmet from 'react-helmet';
 import logo from './logo.svg';
 import './App.css';
 
@@ -6,6 +7,15 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+          <Helmet>
+              <meta charSet="utf-8" />
+              <title>Custom helmet title</title>
+
+              <meta property="og:title" content="Facebook Open Graph META Tags"/>
+              <meta property="og:image" content="https://davidwalsh.name/wp-content/themes/klass/img/facebooklogo.png"/>
+              <meta property="og:site_name" content="David Walsh Blog"/>
+              <meta property="og:description" content="Facebook's Open Graph protocol allows for web developers to turn their websites into Facebook objects, allowing a certain level of customization over how information is carried over from a non-Facebook website to Facebook when a page is 'recommended', 'liked', or just generally shared."/>
+          </Helmet>
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
