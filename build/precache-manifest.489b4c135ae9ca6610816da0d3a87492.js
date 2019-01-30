b364d8751d66da7df567a90658f8b53acd0e82cf
Deniss Ivannikov <denis.butcher@gmail.com>
2019-01-30 11:14:47 +0200

    2

---
+++ build/precache-manifest.489b4c135ae9ca6610816da0d3a87492.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "7878e114517372003d12",
+    "url": "/static/js/main.7878e114.chunk.js"
+  },
+  {
+    "revision": "d27ff06bc19ceeb8731e",
+    "url": "/static/js/1.d27ff06b.chunk.js"
+  },
+  {
+    "revision": "7878e114517372003d12",
+    "url": "/static/css/main.d11d2764.chunk.css"
+  },
+  {
+    "revision": "b0aef2fc4ea1ad0abeeb026f53220163",
+    "url": "/index.html"
+  }
+];
